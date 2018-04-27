9690d1cebadcc3f4f30503836ffc48e0177cb805
jcaruana09 <jcaruana09@outlook.com>
2018-04-27 10:10:04 +0100

    Fixed PORT for heroku

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const port           = process.env.port || 3000;
+const port           = process.env.PORT || 3000;
 const databaseURI    = process.env.MONGODB_URI || 'mongodb://localhost/wdi-project2';
 
 module.exports = {databaseURI, port};
