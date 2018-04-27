d24537f9bfc7caa1a83c0b1b2ee9f388b69e9be7
jcaruana09 <jcaruana09@outlook.com>
2018-04-27 10:01:28 +0100

    Preparing for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const port           = 3000;
-const databaseURI    = 'mongodb://localhost/wdi-project2';
+const port           = process.env.port || 3000;
+const databaseURI    = process.env.MONGODB_URI || 'mongodb://localhost/wdi-project2';
 
 module.exports = {databaseURI, port};
